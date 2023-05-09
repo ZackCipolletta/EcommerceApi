aeb7654eefe4a7a5f26037c3575c344412b57d5b
Zachary <ZackCipolletta@gmail.com>
2023-05-09 11:02:41 -0700

    added CORS

--- EcommerseApi/Program.cs
+++ EcommerseApi/Program.cs
@@ -25,6 +25,12 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseCors(builder => builder
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
